00962517eff15ecb40060d45347fdd8f5317e118
basicomCsharp <coolandrey@gmail.com>
2022-07-24 21:09:12 +0500

    Контрольное задание для модуля 2

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,11 +10,31 @@ namespace nsConsole
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            byte age = (byte)int.Parse(Console.ReadLine());
-            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            var name = "";
+            var age = 0;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            
+            var res = int.TryParse(Console.ReadLine(), out age);
+            
+            while (!res)
+            {
+                Console.WriteLine("What is your age?");
+                res = int.TryParse(Console.ReadLine(), out age);
+            }
+            if (age > 200)
+            {
+                Console.WriteLine("Your name is Dinosaur {0} and age is {1} ", name, age);
+
+            }
+            else
+            {
+                Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            }
+            Console.Write("Write your birthday ");
+            var day = Console.ReadLine();
+            Console.WriteLine("Your birthday is {0}", day);
             Console.ReadKey();
         }
     }
