3fb5bc37b07be2c9f524cb1a5874cb720954e8b7
basicomCsharp <coolandrey@gmail.com>
2022-07-24 20:44:11 +0500

    работа с консолью

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,6 +10,11 @@ namespace nsConsole
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
             Console.ReadKey();
         }
     }
