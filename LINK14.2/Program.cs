c62166bb35e839d08b2b910c0f84531379abdbb6
Andrey Kultyshev <coolandrey@gmail.com>
2022-10-30 16:29:46 +0500

    Задание 15
    Метод, который соберет всех учеников всех классов в один список, используя LINQ.

---
+++ LINK14.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINK14._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+{
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Метод, который соберет всех учеников всех классов в один список, используя LINQ.
+        /// </summary>
+        /// <param name="classes"></param>
+        /// <returns></returns>
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(s => s.Students).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
