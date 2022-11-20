b00dbdf9a4b28b042c6bff7463fd4ce0ddcc2989
Andrey Kultyshev <coolandrey@gmail.com>
2022-11-20 20:35:59 +0500

    SOLID

---
+++ SOLID/CalculatorSalary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    internal class CalculatorSalary:IAccount
+    {
+        // Метод для расчета процентной ставки
+        public void CalculateInterest(Account account)
+        {
+           // расчет процентной ставки зарплатного аккаунта по правилам банка
+           account.Interest = account.Balance * 0.5;
+        }
+    }
+}
---
+++ SOLID/CalculatorSimple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class CalculatorSimple: IAccount
+    {
+        // Метод для расчета процентной ставки
+        public void CalculateInterest(Account account)
+        {
+            // расчет процентной ставки обычного аккаунта по правилам банка
+            account.Interest = account.Balance * 0.4;
+            
+            if (account.Balance < 1000)
+                account.Interest -= account.Balance * 0.2;
+
+            if (account.Balance < 50000)
+                account.Interest -= account.Balance * 0.4;
+        }
+    }
+}
